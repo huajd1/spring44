3706e002f749d64812802bc33d03f16828a5926d
hrrer <130406103+huajd1@users.noreply.github.com>
2023-07-06 00:42:01 +0300

    Update UserServiceImp.java

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -9,22 +9,24 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImp implements UserService {
 
    @Autowired
    private UserDao userDao;
 
+   @Transactional
    @Override
    public void add(User user) {
       userDao.add(user);
    }
 
+   @Transactional
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
    }
 
+   @Transactional
    @Override
    public User getUserByCar(String model, int series) {
       return userDao.getUserByCar(model, series);
